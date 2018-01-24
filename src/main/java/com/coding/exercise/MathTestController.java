8750110a135025f0ef2559f4cce4a9b03489e73b
Adnan A Siddiqui <siddiquia_va@yahoo.com>
2018-01-24 07:41:28 -0500

    Improve code readability, fixed based on requirements

--- src/main/java/com/coding/exercise/MathTestController.java
+++ src/main/java/com/coding/exercise/MathTestController.java
@@ -1,56 +1,91 @@
 package com.coding.exercise;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.coding.exercise.SumTestRequest;
+
 @RestController
 public class MathTestController {
 
-	@RequestMapping(value = "/mathaddquestions", method=RequestMethod.GET)
-	public String getMathAddQuestion() {
-		Random rand = new Random();
-		int numberOfOperands = rand.nextInt(3) + 1;
-		
-		List<Integer> operantList = new ArrayList<Integer>();
-		
-		Integer newNumber = null; 
-		StringBuffer questionText = new StringBuffer("Please sum the numbers ");
-		for (int i=0; i<numberOfOperands; i++) {
-			if (i>0) questionText.append(",");
-			newNumber = rand.nextInt(10) + 1;
-			questionText.append(newNumber);
-		}
-		
-		return questionText.toString();
-	}
+	private static final int MIN_ALLOWED_OPERAND_COUNT = 2;
+	private static final int MAX_ALLOWED_OPERAND_COUNT = 4;
+	private static final int MAX_OPERAND_VALUE = 10;
+	private static final String QUESTION_TEXT = "Please sum the numbers ";
 	
-	@RequestMapping(value="/mathaddquestions", method=RequestMethod.POST, consumes="application/json")
+	@RequestMapping(method=RequestMethod.GET, produces="text/html")
+	public String getMathSumQuestion() {
+
+		return this.generateSummationQuestion();
+	}
+
+	@RequestMapping(method=RequestMethod.POST, consumes="application/json")
 	public ResponseEntity<?> checkMathSum(@RequestBody SumTestRequest request) {
-		String bufferStr = request.getQuestionText();
+
+		int questionOperandSum = getRequestSum(request.getQuestionText());
 		
-		System.out.println(bufferStr);
-		bufferStr = bufferStr.replaceFirst("Please sum the numbers ", "");
-		bufferStr = bufferStr.trim();
-		String[] numberArray = bufferStr.split(",");
-		int finalSum = 0;
-		for (int i=0; i<numberArray.length;i++) {
-			finalSum += Integer.valueOf(numberArray[i]).intValue();
-		}
+		int submittedSum = request.getSuggestedResult().intValue();
 		
-		int resultInRequest = request.getSuggestedResult().intValue();
+		return this.generateResponse(questionOperandSum, submittedSum);
+	}
+
+	private ResponseEntity<String> generateResponse(int requestSum, int submittedSum) {
+		ResponseEntity<String> responseEntity;
 		
-		ResponseEntity responseEntity;
-		if (finalSum != resultInRequest) 
-			responseEntity = ResponseEntity.badRequest().build();
+		if (requestSum != submittedSum) 
+			responseEntity = new ResponseEntity<String>("Incorrect Answer", HttpStatus.BAD_REQUEST);
 		else
-			responseEntity = ResponseEntity.ok().build();
+			responseEntity = new ResponseEntity<String>("Correct Answer", HttpStatus.OK);
+
 		return responseEntity;
 	}
+
+	private int getRequestSum(String questionText) {
+		
+		questionText = questionText.replaceFirst(QUESTION_TEXT, "");
+		questionText = questionText.trim();
+		
+		String[] operandArray = questionText.split(",");
+		
+		int actualSum = 0;
+		for (int i=0; i < operandArray.length; i++) {
+			actualSum += Integer.valueOf(operandArray[i]).intValue();
+		}
+
+		return actualSum;
+	}
+	
+	private String generateSummationQuestion() {
+
+		StringBuffer questionText = new StringBuffer(QUESTION_TEXT);
+		
+		for (int i=0; i < generateOperandCount(); i++) {
+			if (i>0) questionText.append(",");
+			questionText.append(generateOperand());
+		}
+
+		return questionText.toString();
+	}
+
+	private int generateOperandCount() {
+		Random rand = new Random();
+		
+		int operandCount = 0;
+		while (operandCount < MIN_ALLOWED_OPERAND_COUNT) {
+			operandCount = rand.nextInt(MAX_ALLOWED_OPERAND_COUNT) + 1;
+		}
+		return operandCount;
+	}
+
+	private int generateOperand() {
+		Random rand = new Random();
+		return rand.nextInt(MAX_OPERAND_VALUE) + 1;
+	}
+
 }
